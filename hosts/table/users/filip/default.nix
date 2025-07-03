54a84acfc3d2b302530c616e0396549acec105f1
Filip Pękala <272985@student.pwr.edu.pl>
2025-07-03 09:48:04 +0200

    Add i3lock screen wallpaper

--- hosts/table/users/filip/default.nix
+++ hosts/table/users/filip/default.nix
@@ -5,5 +5,6 @@
 		./packages.nix
 		./options
 		./light.nix
+		./modules
 	];
 }
--- hosts/table/users/filip/modules/home-manager/wallpaper.nix
+++ hosts/table/users/filip/modules/home-manager/wallpaper.nix
@@ -1,5 +1,6 @@
 {
 	home-manager.users.filip = {
 		home.file.".wallpapers/chiaki.png".source = ./chiaki.png;
+		home.file.".wallpapers/Bsodwindows10.png".source = ./Bsodwindows10.png;
 	};
 }
