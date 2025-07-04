789af0f126ca5c4c0cecb0711d5b4ea8e523264b
Filip Pękala <272985@student.pwr.edu.pl>
2025-07-04 13:28:04 +0200

    How did this happen wtf

--- hosts/table/options/power-saving.nix
+++ hosts/table/options/power-saving.nix
@@ -1,50 +1,50 @@
-{	config,	options,	lib,	...	}:
+{ config, options, lib, ... }:
 {
-	options	=	{
-		hardware.power_saving	=	lib.mkOption	{
-			type	=	lib.types.bool;
-			default	=	false;
-			example	=	true;
+	options = {
+		hardware.power_saving = lib.mkOption {
+			type = lib.types.bool;
+			default = false;
+			example = true;
 		};
 	};
 
-	config	=
+	config =
 		let
-			cond	=	config.hardware.power_saving;
-		in	{
-			powerManagement.enable	=	true;
+			cond = config.hardware.power_saving;
+		in {
+			powerManagement.enable = true;
 
-			powerManagement.scsiLinkPolicy	=
-				if	cond	then
+			powerManagement.scsiLinkPolicy =
+				if cond then
 					#"med_power_with_dipm"
 					"min_power"
 				else
 					"max_performance";
 
-			powerManagement.cpuFreqGovernor	=
-				if	cond	then
+			powerManagement.cpuFreqGovernor =
+				if cond then
 					"powersave"
 				else
 					"ondemand";
 
-			hardware.nvidia.prime	=
-				if	cond	then
+			hardware.nvidia.prime =
+				if cond then
 					{
-					# offload	=	{
-					# 	enable	=	true;
-					# 	enableOffloadCmd	=	true;
+					# offload = {
+					# 	enable = true;
+					# 	enableOffloadCmd = true;
 					# };
 					# sync.enable = lib.mkForce false;
 				}
 				else
 					{
-					#	sync.enable	=	true;
+					#	sync.enable = true;
 				};
 
-			services.xserver.videoDrivers	=
-				if	cond	then
+			services.xserver.videoDrivers =
+				if cond then
 					[]
 				else
-					[	"nvidia"	];
+					[ "nvidia" ];
 		};
 }
