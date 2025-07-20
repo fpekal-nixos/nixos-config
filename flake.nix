22b09a03ecd317f31ac4b66483f1e17df5a04321
Filip Pękala <272985@student.pwr.edu.pl>
2025-07-20 23:01:59 +0200

    Change formatting in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,89 +1,78 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-		nixosBlankSystem.url = "github:fpekal-nixos/nixos-template";
-		nixosBlankSystem.inputs.nixpkgs.follows = "nixpkgs";
+    nixosBlankSystem.url = "github:fpekal-nixos/nixos-template";
+    nixosBlankSystem.inputs.nixpkgs.follows = "nixpkgs";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		sops-nix = {
-			url = "github:Mic92/sops-nix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		zen-browser = {
-			url = "github:youwen5/zen-browser-flake";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		impurity.url = "github:outfoxxed/impurity.nix";
-		impermanence.url = "github:nix-community/impermanence";
-		boomer.url = "github:fpekal/boomer";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impurity.url = "github:outfoxxed/impurity.nix";
+    impermanence.url = "github:nix-community/impermanence";
+    boomer.url = "github:fpekal/boomer";
 
-		radiod.url = "github:fpekal/radiod";
-		radiod.inputs.nixpkgs.follows = "nixpkgs";
-	};
+    radiod.url = "github:fpekal/radiod";
+    radiod.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-	outputs =
-		{ self, nixosBlankSystem, home-manager, sops-nix, impurity, ... }@inputs:
-		{
-			systems.generic = nixosBlankSystem.systems.minimal.extend {
-				modules = [
-					./default.nix
-					home-manager.nixosModules.home-manager
-					sops-nix.nixosModules.sops
+  outputs =
+    { self, nixosBlankSystem, home-manager, sops-nix, impurity, ... }@inputs: {
+      systems.generic = nixosBlankSystem.systems.minimal.extend {
+        modules = [
+          ./default.nix
+          home-manager.nixosModules.home-manager
+          sops-nix.nixosModules.sops
 
-					{
-						imports = [ impurity.nixosModules.impurity ];
-					}
-				];
+          { imports = [ impurity.nixosModules.impurity ]; }
+        ];
 
-				specialArgs = {
-					inherit inputs;
-				};
-			};
-			
-			systems.everest = self.systems.generic.extend {
-				specialArgs = { host = "everest"; };
-			};
+        specialArgs = { inherit inputs; };
+      };
 
-			systems.table = self.systems.generic.extend {
-				specialArgs = { host = "table"; };
-			};
+      systems.everest =
+        self.systems.generic.extend { specialArgs = { host = "everest"; }; };
 
-			nixosConfigurations.everest = self.systems.everest.mksystem {
-				system = "x86_64-linux";
-				specialArgs = { users = [ "filip" ]; };
+      systems.table =
+        self.systems.generic.extend { specialArgs = { host = "table"; }; };
 
-				modules = [
-					{
-						impurity.enable = false;
-						impurity.configRoot = self;
+      nixosConfigurations.everest = self.systems.everest.mksystem {
+        system = "x86_64-linux";
+        specialArgs = { users = [ "filip" ]; };
 
-						virtualisation.vmVariant.virtualisation = {
-							sharedDirectories = {
-								nixos-config = {
-									source = "/home/filip/dev/nix/configuration";
-									target = "/home/filip/dev/nix/configuration";
-								};
-							};
-						};
-					}
-				];
-			};
+        modules = [{
+          impurity.enable = false;
+          impurity.configRoot = self;
 
-			nixosConfigurations.table = self.systems.table.mksystem {
-				system = "x86_64-linux";
-				specialArgs = { users = [ "filip" ]; };
+          virtualisation.vmVariant.virtualisation = {
+            sharedDirectories = {
+              nixos-config = {
+                source = "/home/filip/dev/nix/configuration";
+                target = "/home/filip/dev/nix/configuration";
+              };
+            };
+          };
+        }];
+      };
 
-				modules = [
-					{
-						impurity.enable = false;
-						impurity.configRoot = self;
-					}
-				];
-			};
-		};
+      nixosConfigurations.table = self.systems.table.mksystem {
+        system = "x86_64-linux";
+        specialArgs = { users = [ "filip" ]; };
+
+        modules = [{
+          impurity.enable = false;
+          impurity.configRoot = self;
+        }];
+      };
+    };
 }
 
