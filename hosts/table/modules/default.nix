204fea2a591e849ee6a5f73c5f0d1ff7a8c0ed02
Filip Pękala <272985@student.pwr.edu.pl>
2025-07-24 18:42:22 +0200

    Add docker

--- hosts/table/modules/default.nix
+++ hosts/table/modules/default.nix
@@ -1,18 +1,19 @@
 {
-	imports = [
-		./boot.nix
-		./dconf.nix
-		./gpu.nix
-		./hardware-configuration.nix
-		./impermanence.nix
-		./locale.nix
-		./programs
-		./gui.nix
-		./networking.nix
-		./wallpapers
-		./bluetooth.nix
-		./specialisations.nix
-		./ssh.nix
-		./hosts.nix
-	];
+  imports = [
+    ./boot.nix
+    ./dconf.nix
+    ./gpu.nix
+    ./hardware-configuration.nix
+    ./impermanence.nix
+    ./locale.nix
+    ./programs
+    ./gui.nix
+    ./networking.nix
+    ./wallpapers
+    ./bluetooth.nix
+    ./specialisations.nix
+    ./ssh.nix
+    ./hosts.nix
+    ./docker.nix
+  ];
 }
