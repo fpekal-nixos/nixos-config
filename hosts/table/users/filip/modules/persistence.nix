63316cacd51d518846f1afb38e7f87578deea3ad
Filip Pękala <272985@student.pwr.edu.pl>
2025-08-23 13:47:11 +0200

    Add games directory

--- hosts/table/users/filip/modules/persistence.nix
+++ hosts/table/users/filip/modules/persistence.nix
@@ -1,24 +1,20 @@
 {
-	environment.persistence."/persistent" = {
-		users.filip = {
-			directories = [
-				".config/vesktop"
-				".local/state/nvim"
-				".local/share/nvim"
-				".local/share/zoxide"
-				".zen"
-				".ssh"
-			];
+  environment.persistence."/persistent" = {
+    users.filip = {
+      directories = [
+        ".config/vesktop"
+        ".local/state/nvim"
+        ".local/share/nvim"
+        ".local/share/zoxide"
+        ".zen"
+        ".ssh"
+        "Games"
+      ];
 
-			files = [
-				".config/sops/age/keys.txt"
-				".bash_history"
-				".gitconfig"
-			];
-		};
-	};
+      files = [ ".config/sops/age/keys.txt" ".bash_history" ".gitconfig" ];
+    };
+  };
 
-	systemd.services."persist-persistent-home-filip-.config-sops-age-keys.txt".before = [
-		"sops-install-secrets.service"
-	];
+  systemd.services."persist-persistent-home-filip-.config-sops-age-keys.txt".before =
+    [ "sops-install-secrets.service" ];
 }
