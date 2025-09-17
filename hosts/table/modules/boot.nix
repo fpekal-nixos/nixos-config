9e53850c74b5b6b170d377da3c26201d2c927804
Filip Pękala <272985@student.pwr.edu.pl>
2025-09-17 19:35:08 +0200

    Detect windows

--- hosts/table/modules/boot.nix
+++ hosts/table/modules/boot.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   # Bootloader.
   #boot.loader.systemd-boot.enable = true;
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     device = "nodev";
+
+    useOSProber = true; # Detect Windows
   };
   boot.loader.efi.canTouchEfiVariables = true;
-
 
   boot.kernelPackages = pkgs.linuxPackages_5_10;
 }
