c2d1747a63cfa1db0511b12862762a71f908b559
Filip Pękala <272985@student.pwr.edu.pl>
2025-09-18 22:21:20 +0200

    Add a spotify with an adblock

--- hosts/table/users/filip/modules/packages.nix
+++ hosts/table/users/filip/modules/packages.nix
@@ -1,15 +1,14 @@
-{ inputs, pkgs, ... }:
-{
-	environment.systemPackages = with pkgs; [
-		inputs.boomer.packages.x86_64-linux.boomer
-		vesktop
-	];
+{ inputs, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    inputs.boomer.packages.x86_64-linux.boomer
+    vesktop
+    (callPackage ../../../packages/spotify.nix { })
+  ];
 
+  home-manager.users.filip.programs.boomer = {
+    enableConfig = true;
 
-	home-manager.users.filip.programs.boomer = {
-		enableConfig = true;
-
-		min_scale = 1.0;
-		scroll_speed = 0.5;
-	};
+    min_scale = 1.0;
+    scroll_speed = 0.5;
+  };
 }
