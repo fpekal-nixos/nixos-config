2bb87abcc740179c95a4880a3526f440d0f31b0f
Filip Pękala <272985@student.pwr.edu.pl>
2026-01-11 19:53:20 +0100

    Add ETS2 mods to persistent memory

--- hosts/table/users/filip/modules/steam.nix
+++ hosts/table/users/filip/modules/steam.nix
@@ -6,4 +6,26 @@
       ];
     };
   };
+
+  environment.persistence."/mnt/hdd/persistent" = {
+    users.filip = {
+      directories = [
+        ".local/share/Euro Truck Simulator 2"
+      ];
+    };
+  };
+
+  # fileSystems."/persistent/home/filip/.local/share/Euro Truck Simulator 2/mod" = {
+  # 	depends = [
+  # 		"/mnt/hdd"
+  # 		"/persistent"
+  # 	];
+
+  # 	device = "/mnt/hdd/Games/ETS-mods";
+  # 	fsType = "none";
+  # 	options = [
+  # 		"bind"
+  # 		"uid=1000"
+  # 	];
+  # };
 }
